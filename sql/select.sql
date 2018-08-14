c066d99fef2dbdb599cbe1faee732eacc2af58bb
LJW <tarpha@gmail.com>
2018-08-14 20:01:39 +0900

    add select.sql

---
+++ sql/select.sql
@@ -0,0 +1,14 @@
+SELECT no
+     , regexp_replace(title, '/(\.torrent|\.mkv|\.mp4|\.tp|\.ts)/', '', 'ig') AS title
+     , link
+     , regexp_replace(rss_title, '/(\.torrent|\.mkv|\.mp4|\.tp|\.ts|360p|720p|1080p)/', '', 'ig') AS rss_title
+     , rss_episode
+     , rss_season
+     , rss_quality
+     , rss_release_group
+     , 0 AS tid
+     , '' AS target 
+FROM  "RSS"
+WHERE  title LIKE concat('%' + title + '%')
+ORDER BY no DESC
+OFFSET $1 LIMIT $2 '
