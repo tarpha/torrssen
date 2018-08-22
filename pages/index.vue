fb9633154d9a4183740f1d1e0ebe089731948137
LJW <tarpha@gmail.com>
2018-08-22 21:46:24 +0900

    footer changes

--- pages/index.vue
+++ pages/index.vue
@@ -32,9 +32,19 @@
     <!-- Modal Component -->
     <nuxt-modal></nuxt-modal>
     <div class="footer">
-      <a href="https://github.com/tarpha/torrssen" target="_blank">
-        <img class="bottom-img" :src="require('~/assets/img/GitHub-Mark-32px.png')" />
-      </a>
+      <div style="text-align: right;">
+        <small>
+          &copy; 2018 TRPE |&nbsp;
+        </small>
+      </div>
+      <div>
+        <a href="https://github.com/tarpha/torrssen" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+        </a>
+        <small>
+          GitHub
+        </small>
+      </div>
     </div>
   </div>
 </template>
@@ -162,14 +172,23 @@ export default {
 }
 
 .footer {
-  background-color: rgba(0, 0, 0, 0.05);
-  height: 4em;
+  background: rgba(0, 0, 0, 0.1);
+  width: 100%;
+  min-height: 4em;
   padding-top: 1em;
+  display: -webkit-flex;
+  -webkit-justify-content: center;
+  display: flex;
+  justify-content: center;
+}
+
+.footer div {
+  -webkit-flex: 1;
+  flex: 1;
 }
 
 .bottom-img {
   display: block;
-  opacity: 0.5;
   margin-left: auto;
   margin-right: auto;
 }
@@ -225,7 +244,7 @@ export default {
 
 /* Transparent Overlay */
 .loading:before {
-  content: '';
+  content: "";
   display: block;
   position: fixed;
   top: 0;
@@ -246,7 +265,7 @@ export default {
 }
 
 .loading:not(:required):after {
-  content: '';
+  content: "";
   display: block;
   font-size: 10px;
   width: 1em;
